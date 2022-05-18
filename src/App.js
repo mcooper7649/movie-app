20e2ee0204675d687c31194b98c35c67d45a140e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-05-18 11:11:18 -0400

    setup Nav, Configured Routes, Added Pages, .Env, updated gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import { Container } from '@mui/material';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="App">feff</div>
-    </>
+      <div className="App">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
