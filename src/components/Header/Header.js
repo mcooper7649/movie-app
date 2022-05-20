8fc5255f820ee5859126399383e403d9b44d1033
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-05-19 23:27:10 -0400

    fixed whitebackground/text issue on singleContent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-  return <span className="header">🎬 Entertainment Hub 🎥 </span>;
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+      🎬 Entertainment Hub 🎥{' '}
+    </span>
+  );
 };
 
 export default Header;
